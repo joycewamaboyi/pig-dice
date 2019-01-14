76c5e42ff14820942987fde4711b0f8843929612
tuyjoy92@yahoo.fr <tuyjoy92@yahoo.fr>
2019-01-14 12:25:27 +0200

    adding funtion

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,15 @@ function Player(name) {
   //Creates our two players
   var playerOne = new Player("Player One");
   var playerTwo = new Player("Player Two");
+  // Dice Constructor (argument = number of sides you want. default = 6)
+  function Dice(sides) {
+    this.sides = sides || 6;
+  }
+  
+  // Roll Prototype - Gets a random number between 1 and 6
+  Dice.prototype.roll = function() {
+    var roll = Math.floor((Math.random() * this.sides ) + 1);
+    if (roll === 1) {
+      alert("Doh! You rolled a 1. Your turn is over.");
+    }
+    return roll;
