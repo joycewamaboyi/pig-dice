2f84dd485fe4bad880d92550144457f33525f8b0
tuyjoy92@yahoo.fr <tuyjoy92@yahoo.fr>
2019-01-14 12:27:22 +0200

    adding funtion

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,18 @@ function Player(name) {
       alert("Doh! You rolled a 1. Your turn is over.");
     }
     return roll;
+}
+  
+// Current Turn Array Prototype
+Player.prototype.addRollToArray = function (x) {
+  this.currentTurnArray.push(x)
+}
+
+// Get sum of Turn Running Total (and exclude 1s from total)
+Player.prototype.sumOfRolls = function() {
+  for (var i = 0; i < this.currentTurnArray.length; i++) {
+    if (this.currentTurnArray[i = this.currentTurnArray.length - 1] === 1) {
+      this.turnRunningScore = this.turnRunningScore;
+    } else {
+      this.turnRunningScore += this.currentTurnArray[i = this.currentTurnArray.length - 1];
+      
